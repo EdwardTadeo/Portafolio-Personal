038b65c5666d5c7ca11c68e6994863c259ab5771
EdwardTadeo <blacksstar98@gmail.com>
2023-02-09 11:55:33 -0500

    Pc-09/02/23

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+import pdf from "../../Assets/../Assets/CV-TADEO ESPINOZA FRANK.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/EdwardTadeo/Portafolio-Personal/master/src/Assets/CV-TADEO ESPINOZA FRANK.pdf";
+  "https://raw.githubusercontent.com/EdwardTadeo/Portafolio-Personal/main/src/Assets/CV-TADEO%20ESPINOZA%20FRANK.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
